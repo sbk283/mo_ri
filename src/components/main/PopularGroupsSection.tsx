dbc9e145f107271b6644701abd760a539f23e7a4
sbkcoding <sbkcoding@gmail.com>
2025-09-16 18:12:12 +0900

    인기모임 탭 생성

--- src/components/main/PopularGroupsSection.tsx
+++ src/components/main/PopularGroupsSection.tsx
@@ -1,5 +1,418 @@
 function PopularGroupsSection() {
-  return <div>Mori 가 선정한 인기모임!</div>;
+  return (
+    <div style={{ margin: '0 auto', maxWidth: '1300px' }}>
+      <div className="inline-block text-start" style={{ marginTop: '40px', padding: '20px' }}>
+        <div className="text-lg">Mo:ri 가 선정한 인기모임!</div>
+        <div className="flex items-center" style={{ marginTop: '8px' }}>
+          <div className="text-xxl">지금 바로 확인하세요!</div>
+          <a href="#" className="text-md text-blue-500" style={{ marginLeft: '50px' }}>
+            더보기 +
+          </a>
+        </div>
+      </div>
+      <div
+        className="flex flex-wrap justify-between"
+        style={{ marginTop: '12px', gap: '12px', padding: ' 0 20px', marginBottom: '40px' }}
+      >
+        <div
+          style={{
+            maxWidth: '300px',
+            height: 'auto',
+            border: '1px solid #eee',
+            borderRadius: '5px',
+            marginTop: '12px',
+          }}
+        >
+          <div
+            className="text-md"
+            style={{
+              backgroundColor: 'red',
+              borderRadius: '15px 15px 15px 0',
+              width: 'fit-content',
+              padding: '4px',
+              color: 'white',
+              textAlign: 'center',
+              fontWeight: 'bold',
+            }}
+          >
+            모집중
+          </div>
+          <div
+            style={{
+              position: 'relative',
+              width: '100%',
+              height: '150px',
+              overflow: 'hidden',
+            }}
+          >
+            <img
+              src="/images/group_img.png"
+              alt="썸네일"
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+            <img
+              src="/images/unfill_star.png"
+              alt="즐겨찾기"
+              style={{
+                position: 'absolute',
+                top: '8px',
+                right: '8px',
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer',
+              }}
+            />
+          </div>
+          <div
+            className="flex justify-between text-sm"
+            style={{ margin: '8px 0', padding: '0 12px' }}
+          >
+            <div style={{ color: 'red' }}>취미/여가</div>
+            <div className="text-gray-400">지역무관</div>
+          </div>
+          <div>
+            <a href="#" className="text-lg" style={{ margin: '0 12px', fontWeight: 'bold' }}>
+              마비노기 던전 공파 모집
+            </a>
+            <div
+              className="text-md"
+              style={{
+                margin: '8px 12px',
+                height: '40px',
+                overflow: 'hidden',
+                marginBottom: '12px',
+                color: '#979797',
+              }}
+            >
+              던전같이돌아요어쩌구저쩌구 빨리와주세요.던전같이돌아요어쩌구저쩌구
+            </div>
+          </div>
+          <div
+            className="flex justify-between text-sm items-center"
+            style={{ margin: '0 12px', marginBottom: '10px' }}
+          >
+            <div
+              style={{
+                backgroundColor: '#b3b3b3',
+                borderRadius: '12px',
+                padding: '4px',
+                lineHeight: '10px',
+              }}
+            >
+              D-3
+            </div>
+            <div
+              style={{
+                backgroundColor: '#e2e2e2',
+                borderRadius: '12px',
+                padding: '4px',
+                lineHeight: '10px',
+              }}
+            >
+              AD
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            maxWidth: '300px',
+            height: 'auto',
+            border: '1px solid #eee',
+            borderRadius: '5px',
+            marginTop: '12px',
+          }}
+        >
+          <div
+            className="text-md"
+            style={{
+              backgroundColor: 'blue',
+              borderRadius: '15px 15px 15px 0',
+              width: 'fit-content',
+              padding: '4px',
+              color: 'white',
+              textAlign: 'center',
+              fontWeight: 'bold',
+            }}
+          >
+            모집예정
+          </div>
+          <div
+            style={{
+              position: 'relative',
+              width: '100%',
+              height: '150px',
+              overflow: 'hidden',
+            }}
+          >
+            <img
+              src="/images/group_img.png"
+              alt="썸네일"
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+            <img
+              src="/images/unfill_star.png"
+              alt="즐겨찾기"
+              style={{
+                position: 'absolute',
+                top: '8px',
+                right: '8px',
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer',
+              }}
+            />
+          </div>
+          <div
+            className="flex justify-between text-sm"
+            style={{ margin: '8px 0', padding: '0 12px' }}
+          >
+            <div style={{ color: 'red' }}>취미/여가</div>
+            <div className="text-gray-400">지역무관</div>
+          </div>
+          <div>
+            <a href="#" className="text-lg" style={{ margin: '0 12px', fontWeight: 'bold' }}>
+              마비노기 모바일 던전 공파 모집
+            </a>
+            <div
+              className="text-md"
+              style={{
+                margin: '8px 12px',
+                height: '40px',
+                overflow: 'hidden',
+                marginBottom: '12px',
+                color: '#979797',
+              }}
+            >
+              던전같이돌아요어쩌구저쩌구 빨리와주세요.던전같이돌아요어쩌구저쩌구
+            </div>
+          </div>
+          <div
+            className="flex justify-between text-sm items-center"
+            style={{ margin: '0 12px', marginBottom: '10px' }}
+          >
+            <div
+              style={{
+                backgroundColor: '#b3b3b3',
+                borderRadius: '12px',
+                padding: '4px',
+                lineHeight: '10px',
+              }}
+            >
+              D-3
+            </div>
+            <div
+              style={{
+                backgroundColor: '#e2e2e2',
+                borderRadius: '12px',
+                padding: '4px',
+                lineHeight: '10px',
+              }}
+            >
+              AD
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            maxWidth: '300px',
+            height: 'auto',
+            border: '1px solid #eee',
+            borderRadius: '5px',
+            marginTop: '12px',
+          }}
+        >
+          <div
+            className="text-md"
+            style={{
+              backgroundColor: 'red',
+              borderRadius: '15px 15px 15px 0',
+              width: 'fit-content',
+              padding: '4px',
+              color: 'white',
+              textAlign: 'center',
+              fontWeight: 'bold',
+            }}
+          >
+            모집중
+          </div>
+          <div
+            style={{
+              position: 'relative',
+              width: '100%',
+              height: '150px',
+              overflow: 'hidden',
+            }}
+          >
+            <img
+              src="/images/group_img.png"
+              alt="썸네일"
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+            <img
+              src="/images/unfill_star.png"
+              alt="즐겨찾기"
+              style={{
+                position: 'absolute',
+                top: '8px',
+                right: '8px',
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer',
+              }}
+            />
+          </div>
+          <div
+            className="flex justify-between text-sm"
+            style={{ margin: '8px 0', padding: '0 12px' }}
+          >
+            <div style={{ color: 'red' }}>취미/여가</div>
+            <div className="text-gray-400">지역무관</div>
+          </div>
+          <div>
+            <a href="#" className="text-lg" style={{ margin: '0 12px', fontWeight: 'bold' }}>
+              마비노기 영웅전 던전 공파 모집
+            </a>
+            <div
+              className="text-md"
+              style={{
+                margin: '8px 12px',
+                height: '40px',
+                overflow: 'hidden',
+                marginBottom: '12px',
+                color: '#979797',
+              }}
+            >
+              던전같이돌아요어쩌구저쩌구 빨리와주세요.던전같이돌아요어쩌구저쩌구
+            </div>
+          </div>
+          <div
+            className="flex justify-between text-sm items-center"
+            style={{ margin: '0 12px', marginBottom: '10px' }}
+          >
+            <div
+              style={{
+                backgroundColor: '#b3b3b3',
+                borderRadius: '12px',
+                padding: '4px',
+                lineHeight: '10px',
+              }}
+            >
+              D-3
+            </div>
+            <div
+              style={{
+                backgroundColor: '#e2e2e2',
+                borderRadius: '12px',
+                padding: '4px',
+                lineHeight: '10px',
+              }}
+            >
+              AD
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            maxWidth: '300px',
+            height: 'auto',
+            border: '1px solid #eee',
+            borderRadius: '5px',
+            marginTop: '12px',
+          }}
+        >
+          <div
+            className="text-md"
+            style={{
+              backgroundColor: 'black',
+              borderRadius: '15px 15px 15px 0',
+              width: 'fit-content',
+              padding: '4px',
+              color: 'white',
+              textAlign: 'center',
+              fontWeight: 'bold',
+            }}
+          >
+            서비스종료
+          </div>
+          <div
+            style={{
+              position: 'relative',
+              width: '100%',
+              height: '150px',
+              overflow: 'hidden',
+            }}
+          >
+            <img
+              src="/images/group_img.png"
+              alt="썸네일"
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+            <img
+              src="/images/unfill_star.png"
+              alt="즐겨찾기"
+              style={{
+                position: 'absolute',
+                top: '8px',
+                right: '8px',
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer',
+              }}
+            />
+          </div>
+          <div
+            className="flex justify-between text-sm"
+            style={{ margin: '8px 0', padding: '0 12px' }}
+          >
+            <div style={{ color: 'red' }}>취미/여가</div>
+            <div className="text-gray-400">지역무관</div>
+          </div>
+          <div>
+            <a href="#" className="text-lg" style={{ margin: '0 12px', fontWeight: 'bold' }}>
+              카드라이더 하실분 모집
+            </a>
+            <div
+              className="text-md"
+              style={{
+                margin: '8px 12px',
+                height: '40px',
+                overflow: 'hidden',
+                marginBottom: '12px',
+                color: '#979797',
+              }}
+            >
+              카트라이더는 서비스 종료했는데 어떻게 같이 하자는거죠? 이해를 할수없네요 어쩌구저쩌구
+            </div>
+          </div>
+          <div
+            className="flex justify-between text-sm items-center"
+            style={{ margin: '0 12px', marginBottom: '10px' }}
+          >
+            <div
+              style={{
+                backgroundColor: '#b3b3b3',
+                borderRadius: '12px',
+                padding: '4px',
+                lineHeight: '10px',
+              }}
+            >
+              D+999
+            </div>
+            <div
+              style={{
+                backgroundColor: '#e2e2e2',
+                borderRadius: '12px',
+                padding: '4px',
+                lineHeight: '10px',
+              }}
+            >
+              AD
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PopularGroupsSection;

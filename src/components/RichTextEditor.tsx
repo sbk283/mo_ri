f05a1e592826dd3e20aabab2f1e49cf08f52abd7
maintainline <z.seon.dev@gmail.com>
2025-09-17 10:22:19 +0900

    [docs] RichTextEditor.tsx 생성

---
+++ src/components/RichTextEditor.tsx
@@ -0,0 +1,14 @@
+// editor 컴포넌트
+
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const RichTextEditor = () => {
+  return (
+    <div>
+      <ReactQuill />
+    </div>
+  );
+};
+
+export default RichTextEditor;
--- src/pages/GroupReviewsPage.tsx
+++ src/pages/GroupReviewsPage.tsx
@@ -1,3 +1,4 @@
+// 모임 후기 관리
 function GroupReviewsPage() {
   return <div>GroupReviewsPage</div>;
 }

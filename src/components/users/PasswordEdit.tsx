15f1e50c1ade92affb4f40804b657a44233b6df9
maintainline <z.seon.dev@gmail.com>
2025-10-02 13:16:08 +0900

    [docs] 일부 수정

--- src/components/users/PasswordEdit.tsx
+++ src/components/users/PasswordEdit.tsx
@@ -198,7 +198,7 @@ function PasswordEdit() {
         </div>
         <div className="border-b border-gray-300 opacity-30 my-[27px]" />
         <div className="flex items-center">
-          <div className="text-lg text-gray-400  font-semibold mr-[60px]">경령사항</div>
+          <div className="text-lg text-gray-400  font-semibold mr-[60px]">경f력사항</div>
 
           {/* 추가된 경력 표시 */}
           <div className="flex flex-col gap-1">
@@ -254,7 +254,7 @@ function PasswordEdit() {
               전체보기
             </Link>
             <div className=" flex gap-[10px] ml-auto">
-              <div>동의 여부</div>
+              <div>전체 동의</div>
               <Switch checked={checked} onChange={value => setChecked(value)} />
             </div>
           </div>

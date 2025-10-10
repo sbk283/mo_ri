81d7f61b530a64a8df9ab89d09300a7f6784691b
maintainline <z.seon.dev@gmail.com>
2025-10-10 18:11:28 +0900

    [docs]문의내역 작업중입니다.

--- src/pages/MyInquiriesPage.tsx
+++ src/pages/MyInquiriesPage.tsx
@@ -19,18 +19,48 @@ function MyInquiriesPage() {
           </div>
         </div>
       </div>
-      {/* 하단 영역 부분 */}
-      <div className="mb-[77px]">
+      {/* 하단 영역 부분  내용 없을때.*/}
+      {/* <div className="mb-[77px]">
         <div className="text-brand font-semibold text-xxl mb-[38px]">1:1 문의 내역</div>
         <div className="w-[1024] border border-gray-300 rounded-[5px] py-[53px] text-center text-xl text-gray-200 font-medium">
           1:1 문의 내역이 없습니다.
         </div>
-      </div>
-      <div>
-        <div className="text-brand font-semibold text-xxl mb-[38px]">1:1 문의 내역 상세보기</div>
-        <div className="w-[1024] border border-gray-300 rounded-[5px] py-[53px] text-center text-xl text-gray-200 font-medium">
-          1:1 문의 내역 상세보기를 눌러 확인하세요.
+      </div> */}
+
+      {/* 하단 영역 부분  내용 있을때.*/}
+      <div className="mb-[77px]">
+        <div className="text-brand font-semibold text-xxl mb-[38px]">1:1 문의 내역</div>
+        <div className="w-[1024px] border border-gray-300 rounded-[5px] text-gray-200 font-medium">
+          <div className="grid grid-cols-6 text-center pt-[14px] gap-[50px]">
+            <div>문의 일자</div>
+            <div>문의 내용</div>
+            <div>문의 유형</div>
+            <div>상태</div>
+            <div>답변일자</div>
+            <div>상세보기</div>
+          </div>
+          <div className="border-b border-black opacity-30 my-[10px] " />
+          <div className="grid grid-cols-6 text-center pb-[14px] gap-[50px]">
+            <div>2025.09.03</div>
+            <div className="truncate">모임 취소 할래요. 어떻게 해요!</div>
+            <div>모임 탐색 / 참여</div>
+            <div>답변대기</div>
+            <div>-</div>
+            <div>상세보기</div>
+          </div>
         </div>
+      </div>
+
+      {/*  1:1 문의내역 상세보기 누르기 전 */}
+      {/* <div className="text-brand font-semibold text-xxl mb-[38px]">1:1 문의 내역 상세보기</div>
+      <div className="w-[1024] border border-gray-300 rounded-[5px] py-[53px] text-center text-xl text-gray-200 font-medium">
+        1:1 문의 내역 상세보기를 눌러 확인하세요.
+      </div> */}
+
+      {/* 1:1 문의 내역 상세보기 눌렀을 때 */}
+      <div className="text-brand font-semibold text-xxl mb-[38px]">1:1 문의 내역 상세보기</div>
+      <div className="w-[1024] border border-gray-300 rounded-[5px]  text-gray-200 font-medium">
+        1:1 문의 내역 상세보기를 눌러 확인하세요.
       </div>
     </MyPageLayout>
   );

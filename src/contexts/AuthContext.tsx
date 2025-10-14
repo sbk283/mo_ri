f829f3e05dc6c8b8c85ec0d375f9226bb2e7c1ba
yachea <dev.yachea@gmail.com>
2025-10-14 18:57:05 +0900

    [docs] 카카오로그인 구글로그인 연동

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -12,8 +12,16 @@ type AuthContextType = {
   signUp: (email: string, password: string) => Promise<{ error?: string }>;
   // 회원 로그인 함수(이메일, 비밀번호) : 비동기라서
   signIn: (email: string, password: string) => Promise<{ error?: string }>;
+  // 카카오 로그인 함수
+  signInWithKakao: () => Promise<{ error?: string }>;
+  // 카카오 계정 연동 해제 함수
+  unlinKakaoAccount: () => Promise<{ error?: string; success?: boolean; message?: string }>;
+  // 구글 로그인 함수
+  signInWithGoogle: () => Promise<{ error?: string }>;
   // 회원 로그아웃
   signOut: () => Promise<void>;
+  //  회원탈퇴기능
+  deleteAccount: () => Promise<{ error?: string; success?: boolean; message?: string }>;
 };
 
 // 2. 인증 컨텍스트 생성 (인증 기능을 컴포넌트에서 활용하게 해줌.)
@@ -68,13 +76,129 @@ export const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
     }
     return {};
   };
+
+  // 카카오 로그인 함수
+  const signInWithKakao: AuthContextType['signInWithKakao'] = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'kakao',
+      // 로그인 실행 후 이동옵션
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
+    // 오류발생시 체크 해보자.
+    if (error) {
+      return { error: error.message };
+    }
+    console.log('카카오 로그인 성공 :', data);
+    return {};
+  };
+
+  const unlinKakaoAccount: AuthContextType['unlinKakaoAccount'] = async () => {
+    try {
+      // 카카오 로그인 사용자인지 확인
+      if (user?.app_metadata.provider !== 'kakao') {
+        return { error: '카카오 로그인 사용자가 아닙니다.' };
+      }
+      // supabase 에서 카카오 계정 연동 해제
+      // 사용자의 카카오 identity 찾기
+      const kakaoIdentity = user.identities?.find(item => item.provider === 'kakao');
+      if (!kakaoIdentity) {
+        return { error: '카카오계정연동 정보를 찾을 수 없습니다.' };
+      }
+      // 사용자의 카카오 identity 찾기 성공
+      const { error } = await supabase.auth.unlinkIdentity(kakaoIdentity);
+      if (error) {
+        console.log('카카오 계정 연동 해제 실패:', error.message);
+        return { error: '카카오 계정 연동 해제에 실패하였습니다.' };
+      }
+      // 계정 해제에 성공했다면
+      return {
+        success: true,
+        message: '카카오 계정 연동이 해제되었습니다. 다시 로그인해주세요.',
+      };
+    } catch (err) {
+      console.log(`카카오 계정 연동 해제 오류:`, err);
+      return { error: '카카오 계정 연동 해제 중 오류가 발생했습니다.' };
+    }
+  };
+
+  // 구글 로그인 함수
+  const signInWithGoogle: AuthContextType['signInWithGoogle'] = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        // 로그인 실행후 이동옵션
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
+    // 오류발생시 체크 해보자.
+    if (error) {
+      return { error: error.message };
+    }
+    console.log('구글 로그인 성공 :', data);
+    return {};
+  };
+
   // 회원 로그아웃
   const signOut: AuthContextType['signOut'] = async () => {
     await supabase.auth.signOut();
   };
 
+  // 회원 탈퇴 기능
+  const deleteAccount: AuthContextType['deleteAccount'] = async () => {
+    try {
+      // 기존에 사용한 데이터들을 먼저 정리한다.
+      const { error: profileError } = await supabase.from('profiles').delete().eq('id', user?.id);
+      if (profileError) {
+        console.log('프로필 삭제 실패', profileError.message);
+        return { error: '프로필 삭제에 실패했습니다.' };
+      }
+      // 탈퇴 신청 데이터 추가
+      // account_deletion_requests 에 Pending 으로 Insert 합니다.
+      // 등록할 삭제 데이터
+      const deleteInfo = {
+        user_email: user?.email as string,
+        user_id: user?.id,
+        reason: '사용자 요청',
+        status: 'pending',
+      };
+      const { error: deleteRequestsError } = await supabase
+        .from('account_deletion_requests')
+        .insert([{ ...deleteInfo }]);
+
+      if (deleteRequestsError) {
+        console.log('탈퇴 목록 추가에 실패:', deleteRequestsError.message);
+        return { error: '탈퇴 목록 추가에 실패했습니다.' };
+      }
+      // 혹시 SMTP 서버가 구축이 가능하다면 관리자에게 이메일 전송하는 자리
+      // 로그아웃 시켜줌.
+      await signOut();
+
+      return {
+        success: true,
+        message: '계정 삭제가 요청되었습니다. 관리자 승인 후 완전히 삭제됩니다.',
+      };
+    } catch (err) {
+      console.log('탈퇴 요청 기능 오류 : ', err);
+      return { error: '계정 탈퇴 처리 중 오류가 발생하였습니다.' };
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ signUp, signIn, signOut, user, session }}>
+    <AuthContext.Provider
+      value={{
+        signUp,
+        signIn,
+        signOut,
+        user,
+        session,
+        signInWithKakao,
+        signInWithGoogle,
+        unlinKakaoAccount,
+        deleteAccount,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,7 @@
-import { Button, Form, Input } from 'antd';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import SignUpBanner from '../components/layout/signup/SignUpBanner';
-
-interface FormValues {
-  email: string;
-  password: string;
-}
+import GoogleLoginButton from '../components/login/GoogleLoginButton';
+import KakaoLoginButton from '../components/login/KakaoLoginButton';
 
 export type FieldData = {
   name: (string | number)[];
@@ -16,18 +12,7 @@ export type FieldData = {
 };
 
 function LoginPage() {
-  const initialValue: FormValues = {
-    email: '',
-    password: '',
-  };
-
-  const onFiledsChange = (field: FieldData[], allFields: FieldData[]) => {
-    console.log(field[0].value);
-  };
-  // 7. 확인 버튼 클릭시 최종 입력값
-  const onFinish = (values: FormValues) => {
-    console.log(values);
-  };
+  const [msg, setMsg] = useState<string>('');
 
   return (
     <div>
@@ -37,105 +22,36 @@ function LoginPage() {
           {/* 왼쪽 */}
           <SignUpBanner />
           {/* 오른쪽 */}
-          <div className="w-[50%] y-full py-[70px] px-[107px]">
-            <div>
-              {/* 로고 */}
-              <div className="flex justify-center items-center mb-12">
-                <img className="w-[110px] h-[35px]" src="/images/mori_logo.svg" alt="Mo:ri" />
+          <div className="w-[50%] y-full py-[160px] px-[107px]">
+            <div className="pr-4">
+              <div className="flex justify-center items-center mb-10 gap-4">
+                <div className="w-[135px] h-[60px]">
+                  <img className="w-full h-full" src="/images/mori_logo.svg" alt="Mo:ri" />
+                </div>
                 <div className="text-lg ml-5">
                   <p>mo:ri 에 오신 걸 환영합니다.</p>
                   <p className="font-bold">로그인 후 더 많은 모임을 만나보세요!</p>
                 </div>
               </div>
-              {/* 입력창 */}
-              <div className="mb-5">
-                <div>
-                  <Form
-                    layout="vertical"
-                    initialValues={initialValue}
-                    onFieldsChange={(field, allFields) => onFiledsChange(field, allFields)}
-                    onFinish={values => onFinish(values)}
-                  >
-                    <div className="mb-[50px]">
-                      <Form.Item
-                        label={<p className="font-bold text-gray-600">이메일 (아이디)</p>}
-                        name={'email'}
-                        required={false}
-                        rules={[
-                          { required: true, message: '아이디를 입력해 주세요.' },
-                          { type: 'email', message: '아이디 형식에 맞지 않습니다.' },
-                        ]}
-                        colon={false}
-                      >
-                        <Input
-                          className="w-[450px] border-t-0 border-l-0 border-r-0 rounded-none border-[#A3A3A3] focus:shadow-none "
-                          placeholder="아이디를 입력해 주세요."
-                        />
-                      </Form.Item>
-                      <Form.Item
-                        label={<p className="font-bold text-gray-600">비밀번호</p>}
-                        name={'password'}
-                        required={false}
-                        rules={[
-                          { required: true, message: '비밀번호를 입력해 주세요.' },
-                          {
-                            pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,16}$/,
-                            message: '비밀번호는 8~16자 이며, 대소문자, 숫자를 포함해야 합니다.',
-                          },
-                        ]}
-                        colon={false}
-                      >
-                        <Input.Password
-                          className="w-[450px] border-t-0 border-l-0 border-r-0 rounded-none border-[#A3A3A3] !focus:shadow-none "
-                          placeholder="비밀번호를 입력하세요."
-                        />
-                      </Form.Item>
-                    </div>
-                    <Form.Item className="mx-auto">
-                      <Button
-                        htmlType="submit"
-                        className="w-[450px] h-[48px] bg-brand text-white text-xl font-bold rounded-[5px]"
-                      >
-                        로그인
-                      </Button>
-                    </Form.Item>
-                  </Form>
-                </div>
-                <div className="flex gap-5 text-sm text-gray-600 justify-end">
-                  <Link className="text-gray-600" to={'/findid'}>
-                    아이디 찾기
-                  </Link>
-                  <span>|</span>
-                  <Link className="text-gray-600" to={'/findpw'}>
-                    비밀번호 찾기
-                  </Link>
-                </div>
+              <div className="border-t py-12 flex flex-col gap-7">
+                <KakaoLoginButton onError={error => setMsg(`카카오 로그인 오류 : ${error}`)} />
+                <GoogleLoginButton
+                  onError={error => setMsg(`구글 로그인 오류 : ${error}`)}
+                  onSuccess={message => setMsg(message)}
+                />
               </div>
-              {/* 다른경로회원가입 */}
-              <div>
-                <div className="w-[450px] border-b border-gray-[#acacac] py-3 text-gray-600 font-bold">
-                  <p>다른 경로 회원가입</p>
-                </div>
-                <div className="flex justify-center items-center py-7 gap-12">
-                  <Link to={'/'} className="flex">
-                    <img className="mr-3 h-[27px]" src="./kakao.svg" alt="카카오로그인" />
-                    <p className=" text-gray-600 font-bold">카카오 로그인</p>
-                  </Link>
-                  <Link to={'/'} className="flex">
-                    <img className="mr-3 h-[27px]" src="./google.svg" alt="구글로그인" />
-                    <p className=" text-gray-600 font-bold">구글 로그인</p>
-                  </Link>
-                </div>
-              </div>
-              {/* 회원가입 */}
-              <div className="text-center py-5">
-                <Link to={'/signup'}>
-                  <p className=" text-gray-600">
-                    아직 회원이 아니신가요?{' '}
-                    <span className="text-brand font-bold">회원가입하기</span>
-                  </p>
-                </Link>
-              </div>
+              {msg && (
+                <p
+                  style={{
+                    marginTop: '5px',
+                    padding: '8px',
+                    backgroundColor: msg.includes('성공') ? 'var(--success-50)' : '#fef2f2',
+                    color: msg.includes('성공') ? '#000' : '#dc2626',
+                  }}
+                >
+                  {msg}
+                </p>
+              )}
             </div>
           </div>
         </div>

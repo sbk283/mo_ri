ee3053965deffaefe106c96ebe845408c60c53c2
yachea <dev.yachea@gmail.com>
2025-11-07 12:43:30 +0900

    [docs] 팝업창 오늘하루안보기 기능끄기

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,16 +1,20 @@
-import MainModal from '../components/common/modal/MainModal';
-import AiGroupsSection from '../components/main/AiGroupsSection';
-import CategorySection from '../components/main/CategorySection';
-import IntroSection from '../components/main/IntroSection';
-import PopularGroupsSection from '../components/main/PopularGroupsSection';
-import ReviewsSection from '../components/main/ReviewsSection';
-import { homeBanners } from '../data/bannerData';
+import MainModal from "../components/common/modal/MainModal";
+import AiGroupsSection from "../components/main/AiGroupsSection";
+import CategorySection from "../components/main/CategorySection";
+import IntroSection from "../components/main/IntroSection";
+import PopularGroupsSection from "../components/main/PopularGroupsSection";
+import ReviewsSection from "../components/main/ReviewsSection";
+import { homeBanners } from "../data/bannerData";
 // 메인 홈 페이지 입니다.
 
 function Index() {
   return (
     <div>
-      <MainModal banners={homeBanners} autoOpen={true} enableDoNotShowToday={true} />
+      <MainModal
+        banners={homeBanners}
+        autoOpen={true}
+        enableDoNotShowToday={false}
+      />
       <IntroSection />
       <CategorySection />
       <PopularGroupsSection />

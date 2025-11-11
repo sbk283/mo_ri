59cb53d51c25288b97c21089812dfa6dcecadaf5
devyubi <dev.munyubigmail.com>
2025-11-11 13:19:20 +0900

    [feat] 텍스트 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -63,7 +63,8 @@ const Header: React.FC = () => {
     const { error } = await supabase.auth.signOut();
     if (error) console.error("로그아웃 실패:", error.message);
     navigate("/");
-    window.location.reload();
+    window.sessionStorage.reload();
+    // window.location.reload();
   };
 
   // 알림 패널 열기/닫기

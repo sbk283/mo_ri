90f1e9cc9a3bb4f75a2c6c82b42290d0e40ba73d
devyubi <dev.munyubigmail.com>
2025-11-11 10:17:14 +0900

    [style] 스켈레톤 색상 변경

--- src/components/skeleton/SkeletonGroupCard.tsx
+++ src/components/skeleton/SkeletonGroupCard.tsx
@@ -1,15 +1,15 @@
 export default function SkeletonGroupCard() {
   return (
-    <div className="animate-pulse flex w-full max-w-[1024px] border border-brand rounded-md bg-gray-100 h-[175px]">
+    <div className="animate-pulse flex w-full max-w-[1024px] border border-[#f5f5f7] rounded-md bg-white h-[175px]">
       {/* 이미지 자리 */}
-      <div className="w-[300px] h-full bg-brand-light" />
+      <div className="w-[300px] h-full bg-gray-100" />
 
       {/* 텍스트 자리 */}
       <div className="flex-1 p-4">
-        <div className="h-5 w-1/4 bg-brand-light rounded mb-3" />
-        <div className="h-4 w-2/3 bg-brand-light rounded mb-2" />
-        <div className="h-4 w-1/2 bg-brand-light rounded mb-2" />
-        <div className="h-4 w-1/3 bg-brand-light rounded" />
+        <div className="h-5 w-1/4 bg-gray-100 rounded mb-3" />
+        <div className="h-4 w-2/3 bg-gray-100 rounded mb-2" />
+        <div className="h-4 w-1/2 bg-gray-100 rounded mb-2" />
+        <div className="h-4 w-1/3 bg-gray-100 rounded" />
       </div>
     </div>
   );
